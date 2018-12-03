7957408037d4727444dcb91f8960b9efea5414df
WeSeewy <1509670254@qq.com>
2018-12-03 18:21:41 +0800

    First commit.

---
+++ app/src/main/java/cn/weseewe/android/weatherapp/Weather.java
@@ -0,0 +1,12 @@
+package cn.weseewe.android.weatherapp;
+
+import java.util.Date;
+
+public class Weather {
+    private String mLoc;
+    private int mTemperature;
+    private Date mDate;
+    private String mCondition;
+
+
+}
