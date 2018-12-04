31921fc1ebca04af829e3ff81891bfbe803673c1
WeSeewy <1509670254@qq.com>
2018-12-04 11:01:30 +0800

    加入数据库和表的各项配置。

--- app/src/main/java/cn/weseewe/android/weatherapp/Weather.java
+++ app/src/main/java/cn/weseewe/android/weatherapp/Weather.java
@@ -4,9 +4,13 @@ import java.util.Date;
 
 public class Weather {
     private String mLoc;
-    private int mTemperature;
+    private String mTmp,mfl;
     private Date mDate;
-    private String mCondition;
+    private String mCond_txt;
+    private String mHum;
+    private String mWind_spd,mWind_dir;//  km/h
+    private String mVis;
+    private String mPres;
 
 
 }
